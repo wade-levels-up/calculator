d9dd510f369481eb6a227657b06eb6754a9827e7
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-26 16:14:12 +1000

    Add press effect for buttons

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const buttons = document.querySelectorAll('button');
-const btnArr = Array.from(buttons);
+let btnArr = Array.from(buttons);
 
 // Grows zero button to be twice as big as a normal button
 let zero = btnArr.find(btn => btn.textContent === '0');
 zero.style.flexGrow = 12;
+
+// add onclick event to all buttons to create click effect
+
+btnArr.map((btn) => {
+    btn.addEventListener('mousedown', ()=> btn.classList.toggle('buttonPress'));
+    btn.addEventListener('mouseup', ()=> btn.classList.toggle('buttonPress'));
+    btn.addEventListener('touchstart', ()=> btn.classList.toggle('buttonPress'));
+    btn.addEventListener('touchend', ()=> btn.classList.toggle('buttonPress'));
+});
