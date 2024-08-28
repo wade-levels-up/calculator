fe775d63ea679e9c87ecbcc7087a2ebda46dadbe
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-28 19:49:41 +1000

    Allow user to input first num operator and sec num
    inputHandler function can now handle whether the
    first number of the equation is being input or the
    second number is being input by looking for when
    the operator is entered, this lets it's switch
    between the first number and second number of the
    equation

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let btnArr = Array.from(buttons);
 let zero = btnArr.find(btn => btn.textContent === '0');
 zero.style.flexGrow = 12;
 
-let equation = { firstValue: 0, nextValue: 0, operand: '' }
+let equation = { firstValue: 0, secondValue: 0, operand: '' };
+let firstNumber = true;
 
 // add event listenever to all buttons to create click effect
 btnArr.map((btn) => {
@@ -33,27 +34,26 @@ btnArr.map((btn) => {
 
 
 // Refreshes screen after input is received
-function setScreen(result) {
+function setScreen() {
     let screen = document.querySelector('#screen');
-    let spread = result.split('');
-    if (spread.length > 9) {
-        screen.textContent = 'Len Exc';
+    if (firstNumber) {
+        screen.textContent = equation.firstValue;
     } else {
-        spread = spread.join('');
-        screen.textContent = spread;
+        screen.textContent = equation.secondValue;
     }
 }
 
 // Clears inputs
 function clear() {
     equation.firstValue = 0;
-    equation.nextValue = 0;
+    equation.secondValue = 0;
     equation.operand = '';
 }
 
 // Positive negative conversion 
-function convertStringToPosOrNeg() {
-    let spread = equation.firstValue.split('');
+function convertStringToPosOrNeg(pos) {
+    
+    let spread = pos.split('');
     if (spread[0] != '-') {
         spread.splice(0, 0, '-');
         spread = spread.join('');
@@ -61,12 +61,18 @@ function convertStringToPosOrNeg() {
         spread.splice(0, 1);
         spread = spread.join('');
     }
-    equation.firstValue = spread;
-    setScreen(equation.firstValue)
+
+    if (firstNumber){
+        equation.firstValue = spread;
+    } else {
+        equation.secondValue = spread;
+    }
+    setScreen();
 }
 
-function addDecimal() {
-    let spread = equation.firstValue.split('');
+function addDecimal(pos) {
+
+    let spread = pos.split('');
     let len = spread.length
     if (spread[len -1] != '.' && !(spread.includes('.'))) {
         spread.push('.');
@@ -74,38 +80,86 @@ function addDecimal() {
     } else {
         spread = spread.join('');
     }
-    equation.firstValue = spread;
-    setScreen(equation.firstValue)
+
+    if (firstNumber){
+        equation.firstValue = spread;
+    } else {
+        equation.secondValue = spread;
+    }
+    setScreen();
 }
 
 function inputHandler(input) {
 
-    // If starting value is zero remove it
-    if (equation.firstValue == 0) {
-        equation.firstValue = '';
-    }
+    console.log({input});
+    console.log({firstNumber});
 
-    // Then, if the input is a number concatenate it to the last number
-    // If it's 0 it'll just reset to it's starting value
-    if (!isNaN(input)) {
-        equation.firstValue += input;
-        setScreen(equation.firstValue);
-    }
+    if (firstNumber === true) {
 
-    // When the input is AC we must reset all the objects values
-    if (input === 'AC') {
-        clear();
-        setScreen('0');
-    }
+        // If starting value is zero remove it
+        if (equation.firstValue == 0) {
+            equation.firstValue = '';
+        }
 
-    if (input === '+/-') {
-        convertStringToPosOrNeg();
-    }
+        if (input === 'AC') {
+            clear();
+            setScreen();
+        }
 
-    if (input === '.') {
-        addDecimal();
-    }
+        // Then, if the input is a number concatenate it to the last number
+        // If it's 0 it'll just reset to it's starting value
+        if (!isNaN(input)) {
+            equation.firstValue += input;
+            setScreen();
+        }
 
+        if (input === '+/-') {
+            convertStringToPosOrNeg(equation.firstValue);
+        }
+
+        if (input === '.') {
+            addDecimal(equation.firstValue);
+        }
+
+        if (input === '+') {
+            equation.operand = '+';
+            firstNumber = false;
+        }
+
+
+    } else if (firstNumber === false) {
+
+            // If starting value is zero remove it
+            if (equation.secondValue == 0) {
+            equation.secondValue = '';
+            }
+    
+            if (input === 'AC') {
+                firstNumber = true;
+                clear();
+                setScreen();
+            }
+    
+            // Then, if the input is a number concatenate it to the last number
+            // If it's 0 it'll just reset to it's starting value
+            if (!isNaN(input)) {
+                equation.secondValue += input;
+                setScreen();
+            }
+    
+            if (input === '+/-') {
+                convertStringToPosOrNeg(equation.secondValue);
+            }
+    
+            if (input === '.') {
+                addDecimal(equation.secondValue);
+            }
+    
+            if (input === '+') {
+                equation.operand = '+';
+                firstNumber = false;
+            }
+    }
 }
 
 
